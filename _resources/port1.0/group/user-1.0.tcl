b47ca8c605209533c8524298806056fde989522d
GA <We.Can.Be.Heroes@yandex.com>
2018-02-16 15:33:20 -0800

    "adding some changes”

--- _resources/port1.0/group/user-1.0.tcl
+++ _resources/port1.0/group/user-1.0.tcl
@@ -6,10 +6,10 @@ set user.ver.perl 5.26
 set user.ver.ruby 2.5
 set user.ver.php 5.4
 set user.ver.python 2.7
-set user.arch "x86_64"
+set user.arch x86_64
 set user.ghub gaming-hacker
-set user.ver.intel "2018.1.126"
-set user.intel.root "/opt/intel/compilers_and_libraries_${user.ver.intel}/mac"
+set user.ver.intel  2018.1.126
+set user.intel.root /opt/intel/compilers_and_libraries_${user.ver.intel}/mac
 set user.tbb.inc  ${user.intel.root}/tbb/inc
 set user.tbb.lib  ${user.intel.root}/tbb/lib
 set user.mkl.root ${user.intel.root}/mkl
@@ -27,17 +27,18 @@ set user.WNO "-Wno-unknown-warning-option -Wno-unused-function -Wno-unused-varia
 set user.MACROS "-DLEVEL1_DCACHE_LINESIZE=64 -DLEVEL1_DCACHE_SIZE=32 -DLEVEL2_DCACHE_SIZE=256 -DHASWELL -DL1_DATA_SIZE=32768 -DL1_DATA_LINESIZE=64 -DL2_SIZE=262144 -DL2_LINESIZE=64 -DDTB_DEFAULT_ENTRIES=64 -DDTB_SIZE=4096 -DHAVE_AVX -DHAVE_AVX2 -DHAVE_CFLUSH -DHAVE_CMOV -DHAVE_FMA3 -DHAVE_MMX -DHAVE_PSE -DHAVE_SSE -DHAVE_SSE2 -DHAVE_SSE3 -DHAVE_SSSE3 -DSSE4_1 -DHAVE_SSE4_2 -DHAVE_VFPV3 -DHAVE_OPENCL -D_REENTRANT -D_THREAD_SAFE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -DHAVE_NASM -DNDEBUG -DPIC -DUSE_PTHREAD"
 
 set universal_variant     no
+set configure.ccache      no
 
 set configure.compiler  macports-clang-${user.ver.clang}
 
 set configure.cc       ${prefix}/bin/clang-mp-${user.ver.clang}
 
-set configure.cflags   ${user.CFLAGS} ${user.WNO} ${user.MACROS} -std=c11 -stdlib=libc++
+set configure.cflags   "${user.CFLAGS} ${user.WNO} ${user.MACROS} -std=c11 -stdlib=libc++"
 
-set configure.cxx      ${prefix}/bin/clang++-mp-${user.ver.clang}
+set configure.cxx      "${prefix}/bin/clang++-mp-${user.ver.clang}"
 
-set configure.cxxflags ${user.CFLAGS} ${user.WNO} ${user.MACROS} -std=c++17 -stdlib=libc++
+set configure.cxxflags "${user.CFLAGS} ${user.WNO} ${user.MACROS} -std=c++17 -stdlib=libc++"
 
-set configure.cpp      ${prefix}/bin/clang-cpp-mp-${user.ver.clang} -E
+set configure.cpp      "${prefix}/bin/clang-cpp-mp-${user.ver.clang} -E"
 
-set user.config.default  "--disable-option-checking  --enable-shared --enable-static --enable-silent-rules --enable-fast-install --disable-dependency-tracking --enable-libtool-lock --disable-debug --enable-simd"
+set user.config.default  "--disable-option-checking  --enable-shared --enable-static  --enable-silent-rules  --enable-fast-install --disable-dependency-tracking --enable-libtool-lock --disable-debug --enable-simd"
