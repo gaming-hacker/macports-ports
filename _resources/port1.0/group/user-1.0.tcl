a74dd069d0279a2f7940644804f7225f3359532e
GA <gaming-hacker@users.noreply.github.com>
2018-09-08 19:22:02 +0200

    adding some changes fixPortfile updated Portfile

--- _resources/port1.0/group/user-1.0.tcl
+++ _resources/port1.0/group/user-1.0.tcl
@@ -1,6 +1,6 @@
 # -*- coding: utf-8; mode: _tcl; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- vim:fenc=utf-8:ft=tcl:et:sw=2:ts=2:sts=2
 
-set user.ver.clang  6.0
+set user.ver.clang  7.0
 set user.ver.gcc    8
 set user.ver.perl   5.26
 set user.ver.ruby   2.5
