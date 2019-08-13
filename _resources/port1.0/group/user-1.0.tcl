b3add885fbcb41a8d3381189dd7ee1d624ca909f
GA <gaming-hacker@users.noreply.github.com>
2019-08-14 00:45:16 +0200

    eupdate 4

--- _resources/port1.0/group/user-1.0.tcl
+++ _resources/port1.0/group/user-1.0.tcl
@@ -1,7 +1,7 @@
 # -*- coding: utf-8; mode: _tcl; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- vim:fenc=utf-8:ft=tcl:et:sw=2:ts=2:sts=2
 
 set user.ver.clang  8.0
-set user.ver.gcc    9
+set user.ver.gcc    10
 set user.ver.perl   5.28
 set user.ver.ruby   2.6
 set ruby.branch     ${user.ver.ruby}
