647bbcb263f2598ef2a3b70eb549c29f39f30295
GA <gaming-hacker@users.noreply.github.com>
2019-08-24 12:47:57 +0200

    major update

---
+++ office/_resources/port1.0/group/waf-1.0.tcl
@@ -0,0 +1,16 @@
+# -*- coding: utf-8; mode: _tcl; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- vim:fenc=utf-8:ft=tcl:et:sw=2:ts=2:sts=2
+
+options waf.python
+default waf.python {${prefix}/bin/python2.7}
+
+depends_build-append    port:python27
+
+configure.cmd           ${waf.python} ./waf configure
+configure.args          --nocache
+
+configure.universal_args-delete --disable-dependency-tracking
+
+build.cmd               ${waf.python} ./waf
+build.target            build
+
+destroot.destdir        --destdir=${destroot}
